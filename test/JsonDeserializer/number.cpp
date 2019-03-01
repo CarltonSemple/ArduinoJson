56bd7a21ed1d66845d3829e3089358edff8c88d4
Benoit Blanchon <github@benoitblanchon.fr>
2019-03-01 09:39:46 +0100

    Reproduce the bug

--- test/JsonDeserializer/number.cpp
+++ test/JsonDeserializer/number.cpp
@@ -5,6 +5,7 @@
 #define ARDUINOJSON_USE_LONG_LONG 0
 
 #include <ArduinoJson.h>
+#include <limits.h>
 #include <catch.hpp>
 
 namespace my {
@@ -15,21 +16,78 @@ using ARDUINOJSON_NAMESPACE::isnan;
 TEST_CASE("deserialize an integer") {
   DynamicJsonDocument doc(4096);
 
-  SECTION("0") {
-    DeserializationError err = deserializeJson(doc, "0");
-    REQUIRE(err == DeserializationError::Ok);
-    REQUIRE(doc.is<int>() == true);
-    REQUIRE(doc.as<int>() == 0);
-    REQUIRE(doc.as<std::string>() == "0");  // issue #808
-  }
+  SECTION("Integer") {
+    SECTION("0") {
+      DeserializationError err = deserializeJson(doc, "0");
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<int>() == true);
+      REQUIRE(doc.as<int>() == 0);
+      REQUIRE(doc.as<std::string>() == "0");  // issue #808
+    }
 
-  SECTION("Negative") {
-    DeserializationError err = deserializeJson(doc, "-42");
+    SECTION("Negative") {
+      DeserializationError err = deserializeJson(doc, "-42");
 
-    REQUIRE(err == DeserializationError::Ok);
-    REQUIRE(doc.is<int>());
-    REQUIRE_FALSE(doc.is<bool>());
-    REQUIRE(doc.as<int>() == -42);
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<int>());
+      REQUIRE_FALSE(doc.is<bool>());
+      REQUIRE(doc.as<int>() == -42);
+    }
+
+#if LONG_MAX == 2147483647
+    SECTION("LONG_MAX") {
+      DeserializationError err = deserializeJson(doc, "2147483647");
+
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<long>() == true);
+      REQUIRE(doc.as<long>() == LONG_MAX);
+    }
+
+    SECTION("LONG_MAX + 1") {
+      DeserializationError err = deserializeJson(doc, "2147483648");
+
+      CAPTURE(LONG_MIN);
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<long>() == false);
+      REQUIRE(doc.is<float>() == true);
+    }
+#endif
+
+#if LONG_MIN == -2147483648
+    SECTION("LONG_MIN") {
+      DeserializationError err = deserializeJson(doc, "-2147483648");
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<long>() == true);
+      REQUIRE(doc.as<long>() == LONG_MIN);
+    }
+
+    SECTION("LONG_MIN - 1") {
+      DeserializationError err = deserializeJson(doc, "-2147483649");
+
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<long>() == false);
+      REQUIRE(doc.is<float>() == true);
+    }
+#endif
+
+#if ULONG_MAX == 4294967295
+    SECTION("ULONG_MAX") {
+      DeserializationError err = deserializeJson(doc, "4294967295");
+
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<unsigned long>() == true);
+      REQUIRE(doc.as<unsigned long>() == ULONG_MAX);
+      REQUIRE(doc.is<long>() == false);
+    }
+
+    SECTION("ULONG_MAX + 1") {
+      DeserializationError err = deserializeJson(doc, "4294967296");
+
+      REQUIRE(err == DeserializationError::Ok);
+      REQUIRE(doc.is<unsigned long>() == false);
+      REQUIRE(doc.is<float>() == true);
+    }
+#endif
   }
 
   SECTION("Floats") {
